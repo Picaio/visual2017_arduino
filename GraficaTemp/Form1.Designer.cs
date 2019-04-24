b9e06d57dae55087b6272f8200eb9408899c1366
Andres <andres_ae90@yahoo.com>
2019-04-24 16:06:56 -0500

    Proyecto realizado en visual stduio 2017

---
+++ GraficaTemp/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace GraficaTemp
+{
+    partial class Temperatura
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(26, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(172, 73);
+            this.groupBox1.TabIndex = 83;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Conexión";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "Puertos COM";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(88, 15);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(71, 21);
+            this.comboBox1.TabIndex = 32;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(86, 39);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(78, 26);
+            this.button5.TabIndex = 31;
+            this.button5.Text = "Desconectar";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 26);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart2
+            // 
+            chartArea1.BackColor = System.Drawing.Color.Black;
+            chartArea1.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea1);
+            this.chart2.Cursor = System.Windows.Forms.Cursors.Default;
+            legend1.Name = "Legend1";
+            this.chart2.Legends.Add(legend1);
+            this.chart2.Location = new System.Drawing.Point(26, 102);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chart2.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Red,
+        System.Drawing.Color.Blue,
+        System.Drawing.Color.Green,
+        System.Drawing.Color.Magenta,
+        System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0))))),
+        System.Drawing.Color.Black,
+        System.Drawing.Color.Yellow,
+        System.Drawing.Color.Purple,
+        System.Drawing.Color.Maroon,
+        System.Drawing.SystemColors.Highlight,
+        System.Drawing.Color.Indigo,
+        System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0))))),
+        System.Drawing.Color.Olive,
+        System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0))))),
+        System.Drawing.Color.LightCoral,
+        System.Drawing.Color.Gold,
+        System.Drawing.Color.GreenYellow,
+        System.Drawing.Color.DarkTurquoise,
+        System.Drawing.Color.DarkKhaki,
+        System.Drawing.Color.DeepPink,
+        System.Drawing.Color.Teal,
+        System.Drawing.Color.SlateBlue};
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.Red;
+            series1.LabelBackColor = System.Drawing.Color.White;
+            series1.LabelBorderColor = System.Drawing.Color.Black;
+            series1.Legend = "Legend1";
+            series1.LegendText = "Temp";
+            series1.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series1.Name = "Series1";
+            this.chart2.Series.Add(series1);
+            this.chart2.Size = new System.Drawing.Size(752, 312);
+            this.chart2.TabIndex = 84;
+            this.chart2.Text = "chart2";
+            this.chart2.PostPaint += new System.EventHandler<System.Windows.Forms.DataVisualization.Charting.ChartPaintEventArgs>(this.chart2_PostPaint);
+            this.chart2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.chart2_MouseDown);
+            this.chart2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.chart2_MouseMove);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(441, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(160, 44);
+            this.textBox1.TabIndex = 85;
+            this.textBox1.Text = "0,00";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(616, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Humedad Relativa";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Temperatura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Temperatura";
+            this.Text = "Temperatura";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ GraficaTemp/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace GraficaTemp
+{
+    public partial class Temperatura : Form
+    {
+
+        System.IO.Ports.SerialPort puerto;
+        String[] listado_puerto = System.IO.Ports.SerialPort.GetPortNames();
+        string datos_puerto;
+        double serie1 = 0.0, serie2 = 0.0;
+        double tiempo = 0.0;
+        int x = 0;
+        int y = 0;
+        string Xg;
+        string Yg;
+        System.Drawing.Rectangle r1 = new System.Drawing.Rectangle();
+        public Temperatura()
+        {
+            InitializeComponent();
+            this.comboBox1.DataSource = listado_puerto; // listado de puertos
+
+        }
+
+        public void serial()
+        {
+
+            try
+            {
+                this.puerto = new System.IO.Ports.SerialPort("" + comboBox1.SelectedItem, 9600, System.IO.Ports.Parity.None, 8, System.IO.Ports.StopBits.One);
+                this.puerto.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(recepcion);
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Verifique:" + System.Environment.NewLine + "- Voltage" + System.Environment.NewLine + "- Conexion del puerto", "Error de puerto COM", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        public void recepcion(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                Thread.Sleep(10);
+                datos_puerto = this.puerto.ReadLine();
+                Console.WriteLine(datos_puerto);
+                if (datos_puerto.StartsWith("$"))
+                { this.Invoke(new EventHandler(actualizar)); }
+
+            }
+
+            catch (Exception) { }
+
+
+        }
+
+     
+   
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tiempo++;
+            chart2.Series[0].Points.AddXY((tiempo/2), serie1);
+            textBox1.Text = ""+ serie2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                serial();
+                puerto.Open();
+                timer1.Start();
+            }
+            catch (Exception)
+            { }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            puerto.Close();
+            timer1.Stop();
+        }
+
+        private void chart2_PostPaint(object sender, System.Windows.Forms.DataVisualization.Charting.ChartPaintEventArgs e)
+        {
+            System.Drawing.Font drawFont = new System.Drawing.Font("Verdana", 8);
+            System.Drawing.SolidBrush drawBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+
+            System.Drawing.StringFormat drawFormat = new System.Drawing.StringFormat();
+            if (!(string.IsNullOrEmpty(Xg) && string.IsNullOrEmpty(Yg)))
+            {
+                e.ChartGraphics.Graphics.DrawString("ELong=" + Xg + "\n" + "Fuerza=" + Yg, drawFont, drawBrush, x + 5, y - 2);
+                e.ChartGraphics.Graphics.DrawRectangle(new Pen(Color.Red, 3), r1);
+            }
+        }
+
+        private void chart2_MouseMove(object sender, MouseEventArgs e)
+        {
+            var pos1 = e.Location;
+            if (prevPosition.HasValue && pos1 == prevPosition.Value)
+                return;
+            tooltip.RemoveAll();
+            prevPosition = pos1;
+            var results1 = chart2.HitTest(pos1.X, pos1.Y, false,
+                                            ChartElementType.DataPoint);
+            foreach (var result1 in results1)
+            {
+                if (result1.ChartElementType == ChartElementType.DataPoint)
+                {
+                    var prop1 = result1.Object as DataPoint;
+                    if (prop1 != null)
+                    {
+                        var pointXPixel = result1.ChartArea.AxisX.ValueToPixelPosition(prop1.XValue);
+                        var pointYPixel = result1.ChartArea.AxisY.ValueToPixelPosition(prop1.YValues[0]);
+
+                        // check if the cursor is really close to the point (2 pixels around)
+
+                        tooltip.Show("Tiempo=" + prop1.XValue + ", Fuerza=" + prop1.YValues[0], this.chart2,
+                                        pos1.X, pos1.Y - 15);
+
+                    }
+                }
+            }
+        }
+        System.Drawing.Point? prevPosition = null;
+        ToolTip tooltip = new ToolTip();
+        private void chart2_MouseDown(object sender, MouseEventArgs e)
+        {
+            chart2.Invalidate();
+            r1.X = x;
+            r1.Y = y;
+            r1.Width = 3;
+            r1.Height = 3;
+        }
+
+        public void actualizar(object s, EventArgs e)
+        {
+            datos_puerto = datos_puerto.Remove(0, 1);
+            string[] arreglo = datos_puerto.Split(';');
+            serie1 = Convert.ToDouble(arreglo[0].Replace(".", ","));
+            serie2 = Convert.ToDouble(arreglo[1].Replace(".", ","));
+        }
+
+
+
+
+
+
+    }
+}
